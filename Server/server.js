8bb6051690745bccc5c6c229a931a15e2aeda3e8
gtlien1 <gtlien1@gmail.com>
2024-02-20 17:40:59 -0600

    added Geoffrey's creative project backend

--- Server/server.js
+++ Server/server.js
@@ -24,6 +24,69 @@ db.connect((err) => {
   }
 })
 
+
+//from Geoffrey's creative project. will need to adjust
+app.post('/login', (req, res) =>{
+  console.log("login request");
+  credentials = req.body.credentials;
+  console.log(credentials);
+  username = credentials.username;
+  password = credentials.password;
+  const sql = `SELECT Password FROM users WHERE Username = ?`;
+  db.query(sql, [username], (err, data) => {
+      console.log(err, data);
+      if(err) return res.json(err);
+      if(password == data[0].Password){
+          return res.json({ success: "true" , message: 'Login successful' });
+      } else {
+          return res.status(401).json({ success: "false", message: 'Invalid username or password' });
+      }
+  })
+})
+
+app.post('/register', (req, res) =>{
+  console.log("register request");
+  credentials = req.body.credentials;
+  console.log(credentials);
+  username = credentials.username;
+  password = credentials.password;
+  const sqlCheckDup = `SELECT Username FROM users WHERE Username = ?`;
+  db.query(sqlCheckDup, [username], (err, data) => {
+      console.log(err, data);
+      if(err) return res.json(err);
+      if(data.length > 0 && username == data[0].Username){
+          console.log("User already exists");
+          return res.json({ message: 'User already exists' });
+      }
+      else{
+          const sql = `INSERT INTO users (Username, Password)  VALUES (?,?)`;
+          db.query(sql, [username,password], (err, data) => {
+              console.log(err, data);
+              if(err) return res.json(err);
+              return res.json({ message: 'Registry successful' });
+          })
+      }
+  })
+})
+
+app.post('/liftentry', (req, res) =>{
+  console.log("lift entry");
+  credentials = req.body.credentials;
+  console.log(credentials);
+  username = credentials.username;
+  lift = credentials.lift;
+  date = credentials.date;
+  weight = credentials.weight;
+  const sql = `INSERT INTO lifts (Username, Lift, Date, Weight) VALUES (?,?,?,?)`;
+  db.query(sql, [username, lift, date, weight], (err, data) => {
+      console.log(err, data);
+      if(err) return res.json(err);
+      return res.json({message: 'Lift input into database' });
+  })
+})
+
+//from Geoffrey's creative project. will need to adjust
+
 db.on('error', function(err) {
     console.log("[mysql error]",err);
 });
