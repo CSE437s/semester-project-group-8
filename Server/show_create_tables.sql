5e6c964a68c7cf3b63c470383af38c9c3d0f83ff
gtlien1 <gtlien1@gmail.com>
2024-02-20 18:33:08 -0600

    updated show create tables for db

--- Server/show_create_tables.sql
+++ Server/show_create_tables.sql
@@ -3,14 +3,14 @@ CREATE DATABASE LiftingApp;
 
 
 --Show create tables
-CREATE TABLE LiftingApp.users (
-	id INT auto_increment NOT NULL,
-	username varchar(100) NOT NULL,
-	password varchar(100) NOT NULL,
-	CONSTRAINT id PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
-
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `goal` varchar(100) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` tinyint(1) DEFAULT NULL,
+  `intensity` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
