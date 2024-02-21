1da7fd5b0f33dbd380a6b45052d8ff874dd4d568
gtlien1 <gtlien1@gmail.com>
2024-02-20 19:05:33 -0600

    fixed show create tables for db

--- Server/show_create_tables.sql
+++ Server/show_create_tables.sql
@@ -14,3 +14,30 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE LiftingApp.Lift (
+	lift_id INT auto_increment NOT NULL,
+	lift_name varchar(100) NOT NULL,
+	CONSTRAINT Lift_PK PRIMARY KEY (lift_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE LiftingApp.Exercise (
+	exercise_id INT auto_increment NOT NULL,
+	user_id INT NOT NULL,
+	lift_id INT NOT NULL,
+	set_num INT NOT NULL,
+	rep_num INT NOT NULL,
+	weight FLOAT NOT NULL,
+	sleep_quality INT NULL,
+	stress_level INT NULL,
+	`date` DATE NOT NULL,
+	CONSTRAINT Exercise_PK PRIMARY KEY (exercise_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
+
+
