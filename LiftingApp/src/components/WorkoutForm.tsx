643fac7bbd058411b26856f5086b802aa8dbd7b5
barasakar <jlinpoughkeepsieday@gmail.com>
2024-02-27 00:00:26 -0600

    I think the workout form works now, although it is ugly

--- LiftingApp/src/components/WorkoutForm.tsx
+++ LiftingApp/src/components/WorkoutForm.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { IonRange, IonList, IonButton, IonItem, IonModal } from '@ionic/react';
-import { useNavigation } from '../hooks/useNavigation'; 
+import { IonInput, IonList, IonButton, IonItem, IonModal } from '@ionic/react';
 import './WorkoutForm.css';
 import { useHistory } from 'react-router';
 
@@ -8,6 +7,7 @@ function WorkoutForm() {
     const [exercises, setExercises] = useState([]);
     const [selectedExercises, setSelectedExercises] = useState([]);
     const [showModal, setShowModal] = useState(false);
+    const [sets, setSets] = useState([]);
     const history = useHistory();
     useEffect(() => {
         fetch('http://localhost:3000/getlifts', {
@@ -25,8 +25,26 @@ function WorkoutForm() {
 
     const selectExercise = (liftName) => {
         setSelectedExercises(currentExercises => [...currentExercises, liftName]);
+        setSets(currentSets => [...currentSets, []]);
         setShowModal(false);
     };
+    const addSet = (exerciseIndex) => {
+        const newSet = { setNumber: sets[exerciseIndex].length + 1, lbs: '', reps: '' };
+        const updatedSets = [...sets];
+        updatedSets[exerciseIndex] = [...updatedSets[exerciseIndex], newSet];
+        setSets(updatedSets);
+    };
+    const updateSet = (exerciseIndex, setIndex, field, value) => {
+        const updatedSets = [...sets];
+        updatedSets[exerciseIndex][setIndex] = { ...updatedSets[exerciseIndex][setIndex], [field]: value };
+        setSets(updatedSets);
+    };
+    const cancelWorkout = () => {
+        setSelectedExercises([]);
+        setSets([]);
+        history.push('/StartWorkout');
+    }
+    
     return (
         <div>
             <IonButton onClick={() => setShowModal(true)}>Add Exercises</IonButton>
@@ -40,15 +58,28 @@ function WorkoutForm() {
                 </IonList>
                 <IonButton onClick={() => setShowModal(false)}>Close</IonButton>
             </IonModal>
-            {selectedExercises.length > 0 && (
-                <div>
-                    <h3>Selected Exercises:</h3>
-                    {selectedExercises.map((exercise, index) => (
-                        <div key={index}>{exercise}</div>
+            {selectedExercises.map((exercise, exerciseIndex) => (
+                <div key={exerciseIndex}>
+                    <h3>{exercise}</h3>
+                    {sets[exerciseIndex].map((set, setIndex) => (
+                        <div key={setIndex}>
+                            Set# {set.setNumber}
+                            <IonInput
+                                value={set.lbs}
+                                placeholder="lbs"
+                                onIonChange={e => updateSet(exerciseIndex, setIndex, 'lbs', e.detail.value)}
+                            />
+                            <IonInput
+                                value={set.reps}
+                                placeholder="Reps"
+                                onIonChange={e => updateSet(exerciseIndex, setIndex, 'reps', e.detail.value)}
+                            />
+                        </div>
                     ))}
+                    <IonButton onClick={() => addSet(exerciseIndex)}>+ Add Sets</IonButton>
                 </div>
-            )}
-            <IonButton onClick={() => history.push('\StartWorkout')}>Cancel Workout</IonButton>
+            ))}
+            <IonButton onClick={cancelWorkout}>Cancel Workout</IonButton>
         </div>
     );
 }
