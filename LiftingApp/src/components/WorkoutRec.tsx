3ab58670e6a11e3c790737f2f797cbd40334e4b9
Samuel Feng <sfeng@wustl.edu>
2024-03-26 16:45:07 -0500

    popup UI changed

--- LiftingApp/src/components/WorkoutRec.tsx
+++ LiftingApp/src/components/WorkoutRec.tsx
@@ -7,17 +7,17 @@ function WorkoutRec({ onAccept, onCancel, liftName, lbs, reps, rpe }) {
         
         <div className="recommendation-container">
             <div className="recommendation-details">
-                <h3>{liftName || 'Recommended Lift'}</h3> 
+                <h3>{liftName || 'Recommended Lift:'}</h3> 
                 <p>{lbs} lbs, {reps} reps</p>
                 <p>RPE: {rpe}</p>
             </div>
             <div className="recommendation-actions">
                 <div className="buttons-container">
-                    <IonButton className="custom-button" expand="block" onClick={onAccept}>
+                    <IonButton className="custom-accept-button" expand="block" onClick={onAccept}>
                         <IonIcon icon={checkmarkOutline} slot="start" />
                         Accept
                     </IonButton>
-                    <IonButton className="custom-button" expand="block" onClick={onCancel}>
+                    <IonButton className="custom-cancel-button" expand="block" onClick={onCancel}>
                         <IonIcon icon={closeOutline} slot="start" />
                         Cancel
                     </IonButton>
