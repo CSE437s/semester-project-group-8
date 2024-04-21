3909fd26c20d1157408b56ec91f2ed8d9b30e610
Samuel Feng <sfeng@wustl.edu>
2024-04-21 16:05:59 -0500

    workoutform changes

--- LiftingApp/src/pages/Workout.tsx
+++ LiftingApp/src/pages/Workout.tsx
@@ -14,15 +14,7 @@ import WorkoutForm from "../components/WorkoutForm";
 const Workout: React.FC = () => {
   return (
     <IonPage>
-      <div>
-        <IonText className="start-workout-text">
-          <h1>Today's Workout</h1>
-        </IonText>
-      </div>
-
       <IonContent className="ion-padding">
-        {/* ADD COMPONENTS + CONTENT... */}
-        <h2>Welcome to your Workout!</h2>
 
         <WorkoutForm />
       </IonContent>
